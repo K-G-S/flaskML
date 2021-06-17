9b6a678a0e8c7477ce46c2ba67b333ce8175b703
Shailesh-05 <66480939+Shailesh-05@users.noreply.github.com>
2021-06-17 15:30:52 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,7 +1,8 @@
 from flask import *
 import os
 from werkzeug.utils import secure_filename
-from keras.models import load_model
+import tensorflow as tf
+from tf.keras.models import load_model
 import numpy as np
 from PIL import Image
 
